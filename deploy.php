3a7baf89f989bd329440d109c9a879a690f6cafa
Jachim Coudenys <jachim.coudenys@combellgroup.com>
2022-11-02 12:46:33 +0100

    Some testing

--- deploy.php
+++ deploy.php
@@ -6,10 +6,12 @@ require 'recipe/symfony.php';
 // Config
 
 set('repository', 'git@github.com:coudenysj/symfony-deployer.git');
+set('http_user', 'jachimbe');
+set('writable_mode', 'chown');
 
-add('shared_files', []);
-add('shared_dirs', []);
-add('writable_dirs', []);
+//add('shared_files', []);
+//add('shared_dirs', []);
+//add('writable_dirs', []);
 
 // Hosts
 
@@ -18,5 +20,11 @@ host('ssh.jachim.be')
     ->set('deploy_path', '~/symfony-deployer');
 
 // Hooks
+
+/*task('deploy:vendors', function () {
+    runLocally('composer install');
+    //run('cd {{release_or_current_path}} && {{bin/composer}} {{composer_action}} {{composer_options}} 2>&1');
+});
+*/
 
 after('deploy:failed', 'deploy:unlock');
